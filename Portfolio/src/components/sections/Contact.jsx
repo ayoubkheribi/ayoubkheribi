22d215b8f775645b16f8902f35dcf39a30316557
Ayoub <akhribi627@gmail.com>
2025-10-12 20:55:25 +0100

    ready to deploy

--- Portfolio/src/components/sections/Contact.jsx
+++ Portfolio/src/components/sections/Contact.jsx
@@ -1,7 +1,42 @@
+import emailjs from 'emailjs-com';
+import { useState } from 'react';
+
+
 export const Contact = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const hundleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY)
+      .then((result) => {
+          alert("Message Sent Successfully");
+          setFormData({ name: '', email: '', message: '' });
+      })
+      .catch(() => alert("An error occurred, Please try again",));
+
+  };
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+    <section className="min-h-screen flex items-center justify-center py-20">
+      <div className="w-150 px-4">
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-center text-transparent">Contact Me</h2>
+        <form className="space-y-6" onSubmit={hundleSubmit}>
+          <div className="relative">
+            <input type="text" id="name" name="name" value={formData.name} required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="Your Name..." onChange={(e) => setFormData({...formData, name: e.target.value}) } />
+          </div>
+          <div className="relative">
+            <input type="email" id="email" name="email" value={formData.email} required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="example@gmail.com" onChange={(e) => setFormData({...formData, email: e.target.value}) }/>
+          </div>
+          <div className="relative">
+            <textarea id="message" name="message" value={formData.message} rows="4" required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="Write Your Message" onChange={(e) => setFormData({...formData, message: e.target.value}) }></textarea>
+          </div>
+          <button type="submit" className="w-full bg-blue-500 text-white font-meduim py-3 px-6 rounded transition relative overflow-hidden hover:-translate-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] ">Send Message</button>
+        </form>
+      </div>
+    </section>
   );
 };
--- Portfolio/src/components/sections/Projects.jsx
+++ Portfolio/src/components/sections/Projects.jsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+
 export const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <section className="min-h-screen flex justify-center items-center py-20">
+      <div className="max-w-5xl mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-center text-transparent ">
+          Featured Projects
+        </h2>
+        <Link to={"https://book-reco-psi.vercel.app/"} target="_blank" rel="noopener noreferrer">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="glass rounded-xl p-6 border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_8px_24px_rgba(59,130,224,0.1)] transition-all">
+            <h3 className="text-xl font-bold mb-2">Book.Reco</h3>
+            <p className="text-gray-400 mb-4">Book.Reco is a web app that lets users search and explore books by title, author, or category. It provides detailed book info using the Google Books API, with a clean and responsive design built for easy browsing.</p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "TailwindCSS", "Zustand", "Axios", "Google Books API"].map((tech) => (
+                <span key={tech} className="bg-blue-500/10 text-blue-500 text-sm px-3 py-1 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+        </Link>
+      </div>
+    </section>
   );
 };
